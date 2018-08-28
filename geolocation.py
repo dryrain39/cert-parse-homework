87a50169d04e7642f85943c0052f22cbd3706156
Geonu Ahn <akwoo1@naver.com>
2018-08-29 08:09:02 +0900

    버그수정

--- geolocation.py
+++ geolocation.py
@@ -13,25 +13,30 @@ if __name__ == "__main__":
     writer = CsvWriter.CsvWriter()
     downloader = FileDownloader.FileDownloader()
 
-    writer.header = ['name', 'time', 'bank_name', 'account_no', 'ip_address', 'country']
+    writer.header = ['name', 'time', 'bank_name',
+                     'account_no', 'ip_address', 'country']
     writer.path = './data.csv'
 
     data = writer.read()
 
-    for i in range(0, len(data)):
-        if (i / 100) == 0:
-            logging.debug("Writing...")
-            writer.write(data, mode='w')
+    writer.path = './data.geolocation.csv'
 
+    for i in range(0, len(data)):
         if data[i][4] == '':
             res = downloader.read(
                 "https://api.ipgeolocation.io/ipgeo?apiKey=&ip={}".format(data[i][4]))
-            
+
             result = json.loads(res)
             logging.debug("Updated {}".format(data[i][4]))
             data[i][5] = result['country_name']
         else:
             logging.debug("Pass {} {}".format(data[i][4], data[i][5]))
 
-    logging.debug("Writing...")
-    writer.write(data, mode='w')
+        writer.write({
+                'name': data[i][0],
+                'time': data[i][1],
+                'bank_name': data[i][2],
+                'account_no': data[i][3],
+                'ip_address': data[i][4],
+                'country': data[i][5]
+            })
