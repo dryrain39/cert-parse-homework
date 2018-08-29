642fd976230aac02031e643324b2ef4077aec62a
Geonu Ahn <akwoo1@naver.com>
2018-08-29 17:51:27 +0900

    예외처리

--- geolocation.py
+++ geolocation.py
@@ -11,11 +11,16 @@ logging.basicConfig(level=logging.DEBUG,
 
 if __name__ == "__main__":
     writer = CsvWriter.CsvWriter()
+    error_writer = CsvWriter.CsvWriter()
     downloader = FileDownloader.FileDownloader()
 
     writer.header = ['name', 'time', 'bank_name',
                      'account_no', 'ip_address', 'country']
     writer.path = './data.csv'
+
+    error_writer.header = ['name', 'time', 'bank_name',
+                     'account_no', 'ip_address', 'country']
+    error_writer.path = './data.err.csv'
 
     data = writer.read()
 
@@ -25,34 +30,33 @@ if __name__ == "__main__":
 
     for i in range(0, len(data)):
         try:
-
             if data[i][5] == '':
                 res = downloader.read(
-                "https://api.ipgeolocation.io/ipgeo?apiKey=&ip={}".format(data[i][4]))
+                    "https://api.ipgeolocation.io/ipgeo?apiKey=&ip={}".format(data[i][4]))
 
                 result = json.loads(res)
-                
+
                 logging.debug("Updated {}".format(data[i][4]))
                 data[i][5] = result['country_name']
-                    
+
             else:
                 logging.debug("Pass {} {}".format(data[i][4], data[i][5]))
 
             writer.write({
-                    'name': data[i][0],
-                    'time': data[i][1],
-                    'bank_name': data[i][2],
-                    'account_no': data[i][3],
-                    'ip_address': data[i][4],
-                    'country': data[i][5]
-                })
+                'name': data[i][0],
+                'time': data[i][1],
+                'bank_name': data[i][2],
+                'account_no': data[i][3],
+                'ip_address': data[i][4],
+                'country': data[i][5]
+            })
 
         except Exception:
             failed.append({
-                    'name': data[i][0],
-                    'time': data[i][1],
-                    'bank_name': data[i][2],
-                    'account_no': data[i][3],
-                    'ip_address': data[i][4],
-                    'country': data[i][5]
-                })
+                'name': data[i][0],
+                'time': data[i][1],
+                'bank_name': data[i][2],
+                'account_no': data[i][3],
+                'ip_address': data[i][4],
+                'country': data[i][5]
+            })
